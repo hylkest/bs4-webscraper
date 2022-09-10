38c5d50c933a99031845070c708445949d24558a
hylkest <76428093+hylkest@users.noreply.github.com>
2022-09-10 17:17:36 +0200

    Added link scraper

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
 # Python webscraper
 
+import requests
+from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import mysql.connector
+import re
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="webscraper"
+)
+
+req = Request("https://google.com")
+html_page = urlopen(req)
+main_link = "https://google.com"
+
+soup = BeautifulSoup(html_page, "html.parser")
+
+links = []
+for link in soup.findAll('a'):
+    links.append(link.get('href'))
+    new_lst = ('"'.join(links))
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO links (main_link, link_scraped) VALUES (%s, %s)"
+    val = (main_link, new_lst)
+    mycursor.execute(sql, val)
+
+new_lst=('"'.join(links))
+print(new_lst)
+
+mycursor = mydb.cursor()
+
+# sql = "INSERT INTO links (main_link, link_scraped) VALUES (%s, %s)"
+# val = (main_link, new_lst)
+# mycursor.execute(sql, val)
+
+mydb.commit()
